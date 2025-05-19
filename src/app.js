3711c9397a375de654a17807645cb8446b4418c4
Kayvon <kayvonwallace@gmail.com>
2025-05-18 20:14:55 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+if (process.env.USER) require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
+
+// TODO: Add your code here
+app.use('/theaters', theatersRouter);
+module.exports = app;
